c54e405f990124de451672fb58d09a008b12fda2
unknown <peter.balcirak@gmail.com>
2019-01-04 19:30:54 +0100

    review

---
+++ project/hw02/interface.cpp
@@ -0,0 +1,10 @@
+#include "interface.hpp"
+
+//=============================================================================
+//
+//  This file here only to make CMake happy in empty solution.
+//
+//  You are NOT required to include this in your final solution.
+//  Feel free to remove it if you feel like doing so.
+//
+//=============================================================================
